f7aaa5489c875195f9171f29d024ca2f33459713
Jenoe Balote <jenoe.balote@mths.ca>
2022-01-26 13:31:07 -0500

    yes

---
+++ call_sheets_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by Jenoe Balote
+# Created on January 2022
+# This program calls Google Sheets API
+
+from datetime import datetime
+from tokenize import String
+from xmlrpc.client import DateTime
+import gspread
+
+# Accessing Google Spreadsheet service account
+sa = gspread.service_account()
+# Accessing Gooogle Spreadsheet file
+sh = sa.open("ReminderClock")
+
+# Accessing sheet within file
+wks = sh.worksheet("Reminder Data")
+
+# Gets Reminder Text
+print(wks.acell('A2').value)
+
+# Gets Time Value from spreadsheet and converts it into datetime value
+print(wks.cell(2, 2).value)
